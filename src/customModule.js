0d2149a015782ed1f758abdf036367f7bd166180
woonseok105 <yws3786@gmail.com>
2024-01-09 09:20:02 +0900

    use module

---
+++ src/customModule.js
@@ -0,0 +1,5 @@
+export function sum(max) {
+    return (max + 1) * max / 2;
+}
+
+export const var1 = 'NEW VALUE 100';
---
+++ src/home.js
@@ -0,0 +1,5 @@
+import {sum, var1} from './customModule.js'
+
+console.log(`sum = ${sum(100)}`);
+
+console.log(`var1 = ${var1}`);
